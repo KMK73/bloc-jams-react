f9936dca4155ad676ec59b253c37833d39b1dc05
Kelsey Kjeldsen <kelsey.kjeldsen@gmail.com>
2017-12-22 00:32:12 -0500

    library page styles

--- src/App.js
+++ src/App.js
@@ -10,28 +10,30 @@ class App extends Component {
     return (
       <div className="App">
 
-        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-          <header class="mdl-layout__header">
-            <div class="mdl-layout__header-row">
-              <span class="mdl-layout-title">Bloc Jams</span>
+        <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+          <header className="mdl-layout__header">
+            <div className="mdl-layout__header-row">
+              <span className="mdl-layout-title">
+                <img src="./../assets/images/bloc_jams_logo.png" alt="logo" />
+              </span>
 
-              <div class="mdl-layout-spacer"></div>
+              <div className="mdl-layout-spacer"></div>
 
-              <nav class="mdl-navigation mdl-layout--large-screen-only">
+              <nav className="mdl-navigation mdl-layout--large-screen-only">
                 <Link to='/' className="mdl-navigation__link">Landing</Link>
                 <Link to='/library' className="mdl-navigation__link">Library</Link>
               </nav>
             </div>
           </header>
-          <div class="mdl-layout__drawer">
-            <span class="mdl-layout-title">Bloc Jams</span>
-            <nav class="mdl-navigation">
+          <div className="mdl-layout__drawer">
+            <span className="mdl-layout-title">Bloc Jams</span>
+            <nav className="mdl-navigation">
               <Link to='/' className="mdl-navigation__link">Landing</Link>
               <Link to='/library' className="mdl-navigation__link">Library</Link>
             </nav>
           </div>
-          <main class="mdl-layout__content">
-            <div class="page-content">
+          <main className="mdl-layout__content">
+            <div className="page-content">
               <Route exact path="/" component={Landing} />
               <Route path="/library" component={Library} />
               <Route path="/album/:slug" component={Album}/>
--- src/components/Library.js
+++ src/components/Library.js
@@ -11,9 +11,14 @@ class Library extends Component {
       };
     }
 
+
     render() {
       return (
         <section className='library'>
+          <section>
+            <h3>Music Collection</h3>
+          </section>
+          <div className="mdl-grid">
           {
             this.state.albums.map(
               (album, index) =>
@@ -21,16 +26,31 @@ class Library extends Component {
               //template literal to build a
               // unique link path for each album based on a base URL,
               // /album/ and the album's slug property.
-
-                <Link to={`/album/${album.slug}`} key={index}>
-                  <img src={album.albumCover} alt={album.title} />
-                  <div>{album.title}</div>
-                  <div>{album.artist}</div>
-                  <div>{album.songs.length} songs</div>
+              <div className="mdl-cell  mdl-cell--6-col mdl-cell--12-col-tablet  mdl-cell--12-col-phone"  key={index}>
+                <Link to={`/album/${album.slug}`}>
+                  <div className="card-wide mdl-card mdl-shadow--2dp" style={{background: "url(" + album.albumCover + ") center / cover"}}>
+                    <div className="mdl-card__title">
+                      <h2 className="mdl-card__title-text">{album.title}</h2>
+                    </div>
+                    <div className="mdl-card__supporting-text">
+                      <div className="float-left">
+                        <h4>{album.artist}</h4>
+                      </div>
+                      <div className="float-right">
+                        <h5>Songs: {album.songs.length}</h5>
+                      </div>
+                    </div>
+                    <div className="mdl-card__actions mdl-card--border">
+                      <div className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                        Go To Album
+                      </div>
+                    </div>
+                  </div>
                 </Link>
-
+              </div>
             )
           }
+          </div>
         </section>
       );
     }
