5fdcf70c8ca48f3d1aa4b74434da0ab2f68fc147
Kelsey Kjeldsen <kelsey.kjeldsen@gmail.com>
2017-12-21 23:14:06 -0500

    nav styles set

--- src/App.js
+++ src/App.js
@@ -9,18 +9,35 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <nav>
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
-          </nav>
-          <h1>Bloc Jams</h1>
-        </header>
-        <main>
-          <Route exact path="/" component={Landing} />
-          <Route path="/library" component={Library} />
-          <Route path="/album/:slug" component={Album}/>
-        </main>
+
+        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+          <header class="mdl-layout__header">
+            <div class="mdl-layout__header-row">
+              <span class="mdl-layout-title">Bloc Jams</span>
+
+              <div class="mdl-layout-spacer"></div>
+
+              <nav class="mdl-navigation mdl-layout--large-screen-only">
+                <Link to='/' className="mdl-navigation__link">Landing</Link>
+                <Link to='/library' className="mdl-navigation__link">Library</Link>
+              </nav>
+            </div>
+          </header>
+          <div class="mdl-layout__drawer">
+            <span class="mdl-layout-title">Bloc Jams</span>
+            <nav class="mdl-navigation">
+              <Link to='/' className="mdl-navigation__link">Landing</Link>
+              <Link to='/library' className="mdl-navigation__link">Library</Link>
+            </nav>
+          </div>
+          <main class="mdl-layout__content">
+            <div class="page-content">
+              <Route exact path="/" component={Landing} />
+              <Route path="/library" component={Library} />
+              <Route path="/album/:slug" component={Album}/>
+            </div>
+          </main>
+        </div>
       </div>
     );
   }
--- src/components/Library.js
+++ src/components/Library.js
@@ -23,27 +23,6 @@ class Library extends Component {
               // /album/ and the album's slug property.
 
                 <Link to={`/album/${album.slug}`} key={index}>
-                  <div className="demo-card-wide mdl-card mdl-shadow--2dp">
-                    <div className="mdl-card__title">
-                      <h2 className="mdl-card__title-text">Welcome</h2>
-                    </div>
-
-                    <div className="mdl-card__supporting-text">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Mauris sagittis pellentesque lacus eleifend lacinia...
-                    </div>
-                    <div className="mdl-card__actions mdl-card--border">
-                      <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                        Get Started
-                      </a>
-                    </div>
-                    <div className="mdl-card__menu">
-                      <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-                        <i className="material-icons">share</i>
-                      </button>
-                    </div>
-                  </div>
-
                   <img src={album.albumCover} alt={album.title} />
                   <div>{album.title}</div>
                   <div>{album.artist}</div>
