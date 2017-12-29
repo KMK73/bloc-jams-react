0d550a732e58c6b6f031123995501434c28cf8f4
Kelsey Kjeldsen <kelsey.kjeldsen@gmail.com>
2017-12-28 22:43:44 -0500

    landing styles

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,21 +6,48 @@ const Landing = () => (
 
       <section className="selling-points mdl-grid">
 
-        <div className="point mdl-cell--4-col mdl-cell--12-col-tablet  ">
-          <img src="/assets/images/background1.jpg" alt="music" />
-          <h2 className="point-title">Choose your music</h2>
-          <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+        <div class="demo-card-square mdl-card mdl-shadow--2dp">
+          <div id="card1" class="mdl-card__title mdl-card--expand">
+            <h2 class="mdl-card__title-text">Choose your music</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            The world is full of music; why should you have to listen to music that someone else chose?
+          </div>
+          <div class="mdl-card__actions mdl-card--border">
+            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="/library">
+              View Music
+            </a>
+          </div>
         </div>
-        <div className="point mdl-cell--4-col mdl-cell--12-col-tablet ">
-            <img src="/assets/images/background2.jpg" alt="music" />
-          <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-          <p className="point-description">No arbitrary limits. No distractions.</p>
+
+        <div class="demo-card-square mdl-card mdl-shadow--2dp">
+          <div id="card2" class="mdl-card__title mdl-card--expand">
+            <h2 class="mdl-card__title-text">Unlimited, streaming, ad-free</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+          No arbitrary limits. No distractions.
+          </div>
+          <div class="mdl-card__actions mdl-card--border">
+            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="/library">
+              View Music
+            </a>
+          </div>
         </div>
-        <div className="point mdl-cell--4-col mdl-cell--12-col-tablet ">
-            <img src="/assets/images/mobile.jpg" alt="music" />
-          <h2 className="point-title">Mobile enabled</h2>
-          <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+
+        <div class="demo-card-square mdl-card mdl-shadow--2dp">
+          <div id="card3" class="mdl-card__title mdl-card--expand">
+            <h2 class="mdl-card__title-text">Mobile enabled</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            Listen to your music on the go. This streaming service is available on all mobile platforms.
+          </div>
+          <div class="mdl-card__actions mdl-card--border">
+            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="/library">
+                View Music
+            </a>
+          </div>
         </div>
+
       </section>
 
     </section>
